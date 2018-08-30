693c4b3a9e0f927e2ac4cf658ec042f1241676a1
jrmr <jeremierobles@gmx.fr>
2018-08-30 04:50:32 +0200

    laste update, ex04 ifs finish

--- day02/ex04/ft_print_comb.c
+++ day02/ex04/ft_print_comb.c
@@ -6,47 +6,37 @@
 /*   By: jrobles <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 22:50:12 by jrobles           #+#    #+#             */
-/*   Updated: 2018/08/30 02:15:16 by jrobles          ###   ########.fr       */
+/*   Updated: 2018/08/30 04:46:06 by jrobles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
 
 int		ft_putchar(char c);
 void	ft_print_comb(void);
 
-int main (void)
-{
-	ft_print_comb();
-	return (0);
-}
-
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
 	int c;
 	int d;
 	int u;
-	c = 0;
-	d = 1;
-	u = 2;
-	while(c<8)
-	{
-		while(d<9)
-		{
-			ft_putchar(d);
-			while(u<10)
-			{
-				ft_putchar(u);
-				u++;
-			}
-			d++;
-		}
-		c++;	
-	}
-}
 
-int    ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (0);
+	c = -1;
+	while (c++ < 7)
+	{
+		d = c;
+		while (d++ < 8)
+		{
+			u = d;
+			while (u++ < 9)
+			{
+				ft_putchar(c % 10 + '0');
+				ft_putchar(d % 10 + '0');
+				ft_putchar(u % 10 + '0');
+				if (c != 7)
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+			}
+		}
+	}
 }
