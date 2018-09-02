e58f21acfe039797cf7b8b3198534adfd6db9f37
jrmr <jeremierobles@gmx.fr>
2018-09-02 18:08:32 +0200

    error 42
    error negative values

--- rush00/ex00/rush00.c
+++ rush00/ex00/rush00.c
@@ -6,7 +6,7 @@
 /*   By: jrobles <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/01 00:26:17 by jrobles           #+#    #+#             */
-/*   Updated: 2018/09/01 13:58:57 by jrobles          ###   ########.fr       */
+/*   Updated: 2018/09/02 11:31:15 by jrobles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@ int		ft_putchar(char c);
 void	draw(int size, char img1, char img2);
 
 void	rush(int x, int y)
-{	
-	if(y > 2)
+{
+	if (y > 2)
 	{
 		draw(x, 'o', '-');
 		while (y-- - 2)
-			draw(x, '|', ' ');
+			draw (x, '|', ' ');
 		draw(x, 'o', '-');
 	}
 	else
 	{
-		while(y--)
+		while (y--)
 		{
 			draw(x, 'o', '-');
 		}
@@ -33,23 +33,23 @@ void	rush(int x, int y)
 
 void	draw(int size, char img1, char img2)
 {
-		if(size > 2)
+	if (size > 2)
+	{
+		ft_putchar(img1);
+		while (size-- - 2)
 		{
-			ft_putchar(img1);
-			while(size-- - 2)
+			ft_putchar(img2);
+			if (( size - 2 ) < 1  )
 			{
-				ft_putchar(img2);
-				if((size -2) < 1  )
-				{
-					ft_putchar(img1);
-					ft_putchar('\n');
-				}
+				ft_putchar(img1);
+				ft_putchar('\n');
 			}
 		}
-		else
-		{
-			while(size--)
-				ft_putchar(img1);
-			ft_putchar('\n');
-		}
-}	
+	}
+	else
+	{
+		while (size--)
+			ft_putchar(img1);
+		ft_putchar('\n');
+	}
+}
