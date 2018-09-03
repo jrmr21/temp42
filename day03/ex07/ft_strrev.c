9330018d711ca2356ae73d9f75bda69e0ac39d01
jrmr <jeremierobles@gmx.fr>
2018-09-03 17:36:07 +0200

    final ex07

--- day03/ex07/ft_strrev.c
+++ day03/ex07/ft_strrev.c
@@ -6,60 +6,39 @@
 /*   By: jrobles <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/02 03:11:59 by jrobles           #+#    #+#             */
-/*   Updated: 2018/09/03 13:33:35 by jrobles          ###   ########.fr       */
+/*   Updated: 2018/09/03 17:34:16 by jrobles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 char	*ft_strrev(char *str);
-int     ft_strlen(char *str);
+int		ft_strlen(char *str);
 
 char	*ft_strrev(char *str)
 {
-	int size;
-	int i;
-	char tempo;
-	
-	size = ft_strlen(str) ;
-	i = 0;
-	while (i <= size)
+	int		size;
+	int		i;
+	char	*tempo;
+	char	prout[*str];
+
+	i = -1;
+	size = ft_strlen(str);
+	while (i++ <= size--)
 	{
-		
-		tempo = str[size];
-		str[size] = str[i];
-		str[i] = tempo;
-		i++;
-		size--;
+		prout[i] = str[size];
+		prout[size] = str[i];
 	}
-	return (str);	
+	tempo = prout;
+	return (tempo);
 }
 
-int     ft_strlen(char *str)
+int		ft_strlen(char *str)
 {
 	int out;
-	
+
 	out = 0;
-	while (*str)
-	{
+	while (*str++)
 		out++;
-	}
 	return (out);
-}
-
-int main(void)
-{
-	char *mot;
-	char tmp;
-
-	mot = "titi et gros minet";
-	ft_strrev(mot);
-
-	while(*mot - 1)
-	{
-		tmp = *mot++;
-		write(1, &tmp, 1);
-	}
-	return 0;
 }
