c1c63187c157ab9e2469bb73cbd36cc7e171e997
jrmr <jeremierobles@gmx.fr>
2018-09-11 18:51:40 +0200

    w4y

--- day05/ex07/ft_strncmp.c
+++ day05/ex07/ft_strncmp.c
@@ -1,23 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrobles <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/11 11:06:14 by jrobles           #+#    #+#             */
-/*   Updated: 2018/09/11 18:17:40 by jrobles          ###   ########.fr       */
+/*   Created: 2018/09/11 18:21:12 by jrobles           #+#    #+#             */
+/*   Updated: 2018/09/11 18:22:08 by jrobles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strcmp(char *s1, char *s2);
+#include <stdio.h>
 
-int ft_strcmp(char *s1, char *s2)
-{	
-	while( *s1 == *s2 && *s1 != '\0')
+int ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int cpt;
+
+	cpt = 0;
+	printf("%s  	%s\n",s1,s2 );
+	while( *s1 == *s2 && *s1 != '\0' && cpt < n)
 	{
-		*s2 += 1;
-		*s1 += 1;
+		*s2+=1;
+		*s1+=1;
+		cpt++;
 	}
 	return (*s1 - *s2);
+}
+
+int		main(void)
+{
+	char g[] = "attG";
+	char h[] = "attH";
+	printf("%d ", ft_strncmp(g,h,3));
+	return (0);
 }
