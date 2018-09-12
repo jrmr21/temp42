cf4a2e13823ec9cce294f81898d703bede756d15
jrmr <jeremierobles@gmx.fr>
2018-09-12 02:23:24 +0200

    update up char starting letter

---
+++ day05/ex10/ft_strcapitalize.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrobles <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/11 22:35:43 by jrobles           #+#    #+#             */
+/*   Updated: 2018/09/11 22:44:24 by jrobles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcapitalize(char *str);
+char	ft_strlowcase(char str);
+char	ft_strupcase(char str);
+
+char	ft_charupcase(char str)
+{
+	if (str < 123 && str > 96)
+		str = str - 32;
+	return (str);
+}
+
+char	ft_charlowcase(char str)
+{
+	if (str < 91 && str > 64)
+		str = str + 32;
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	char maj;
+	int i;
+
+	i 	= -1;
+	maj = 1;
+	while (str[++i] != '\0')
+	{
+		if ((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123))
+		{
+			if (maj)
+			{
+				maj = 0;
+				str[i] = ft_charupcase(str[i]);
+			}
+			else
+				str[i] = ft_charlowcase(str[i]);
+		}
+		else if (str[i] < 48 || str[i] > 57)
+			maj = 1;
+		else if (str[i] > 47 || str[i] < 58)
+			maj = 0;
+	}
+	return (str);
+}
+
+int		main(void)
+{
+	char b[100] ="salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	printf("\n %s",ft_strcapitalize(b));
+	return (0);	
+}
