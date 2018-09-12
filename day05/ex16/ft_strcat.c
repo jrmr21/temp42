64a67245728599deb28a1c7363007339acffa0db
jrmr <jeremierobles@gmx.fr>
2018-09-12 11:58:48 +0200

    update

--- day05/ex16/ft_strcat.c
+++ day05/ex16/ft_strcat.c
@@ -6,27 +6,27 @@
 /*   By: jrobles <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/12 10:52:42 by jrobles           #+#    #+#             */
-/*   Updated: 2018/09/12 10:52:46 by jrobles          ###   ########.fr       */
+/*   Updated: 2018/09/12 11:56:20 by jrobles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strcat(char *dest, char *src);
 
-
-char *ft_strcat(char *dest, char *src);
-
-char *ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dest, char *src)
 {
-	int 
+	int i;
+	int b;
 
-	reurn 0;
-}
-
-int main(void)
-{
-	char b[100] = "salut";
-	char c[100] = " ca va ?";
-
-	ft_strcat(b,c);
-	printf("%s ",b)
-	return 0;
+	b = 0;
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[b] != '\0')
+	{
+		dest[i] = src[b];
+		i++;
+		b++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
